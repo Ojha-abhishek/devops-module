6e11fcf2d3a4c663ecce65df604ec48cec57e776
shivams65 <shivams65@gmail.com>
2023-09-25 02:45:00 +0530

    datasource edit

--- Linux_VM/datasource.tf
+++ Linux_VM/datasource.tf
@@ -2,7 +2,7 @@ data "azurerm_subnet" "subnet" {
   for_each             = var.linuxvm
   name                 = each.value.subnetname
   virtual_network_name = each.value.virtual_network_name
-  resource_group_name  = "shivam-rg"
+  resource_group_name  = each.value.resource_group_name
 }
 
 data "azurerm_key_vault" "keyvault" {
